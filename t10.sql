c178449b750027d96b4260e2497c285df3963f74
greenPanther824 <72186256+greenPanther824@users.noreply.github.com>
2021-01-25 10:51:28 +0530

    Update t10.sql

--- t10.sql
+++ t10.sql
@@ -1,3 +1,16 @@
+CREATE TABLE Employee(
+    code CHAR(6) PRIMARY KEY,
+    name VARCHAR(80),
+    dob date,
+    designation VARCHAR(100),
+    salary FLOAT
+);
+INSERT INTO Employee (code, name, dob, designation, salary)
+VALUES
+('gm0121','John','2000-10-05','General Manager',50000.25),
+('pm2918','Ludo','1998-11-15','Product Manager',50000.50),
+('cl0123','ISHKA','1992-02-06','Clerk',25000),
+('cl1234','SASI','2001-02-05','Clerk',15000.75);
 Create Table CUR
 (name char(10),
  dob date,
